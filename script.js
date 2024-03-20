087f633aa6a09d64b78f06472c19d69b748bb55c
Nitin gour <gournitin39@gmail.com>
2024-03-20 17:50:41 +0530

    first git file

---
+++ script.js
@@ -0,0 +1,20 @@
+const element=document.querySelectorAll(".element");
+
+element.forEach(function(ele){
+    // console.log(ele.childNodes[3])
+    ele.addEventListener("mouseenter",function(e){
+        console.log(e.x)
+        ele.childNodes[3].style.opacity=1;
+    })
+
+    ele.addEventListener("mouseover",function(e){
+        ele.childNodes[3].style.left=`${e.x}px`;
+    })
+
+    ele.addEventListener("mouseleave",function(e){
+        ele.childNodes[3].style.opacity=0;
+    })
+
+
+
+})
